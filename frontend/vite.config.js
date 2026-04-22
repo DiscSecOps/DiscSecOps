7635aa23f7b0526dc1e6028a3dcf1e328d043c1a
Camelia Ciuca <159022736+Cameloasa@users.noreply.github.com>
2026-04-22 17:05:00 +0200

    Update vite.config.js
    update vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend/',   // 🔥 necesar pentru GitHub Pages
   plugins: [react()],
   server: {
     host: '0.0.0.0', // host for container
